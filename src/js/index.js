c7e567431efe6136e950c603eef50da66c0be35b
Yoshihisa Suzuki <mf37459@gmail.com>
2019-02-23 08:18:20 +0900

    微調整

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,12 @@ $(function () {
   $('a[href^="#"').click(function () {
     const speed = 500;
     const href = $(this).attr("href");
+    if (href === "#top") {
       const target = $((href === "#" || href === "") ? 'html' : href);
       const position = target.offset().top;
       $("html, body").animate({ scrollTop: position }, speed, "swing");
       return false;
+    }
   })
 });
 
