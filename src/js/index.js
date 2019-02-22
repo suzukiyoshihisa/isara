0e29e0bc6f3bf1db5c71a1173835f389bc37ad38
Yoshihisa Suzuki <mf37459@gmail.com>
2019-02-23 08:12:55 +0900

    PCの動きまで

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
 const $ = require('jquery');
+
+// スムーススクロール
+$(function () {
+  $('a[href^="#"').click(function () {
+    const speed = 500;
+    const href = $(this).attr("href");
+      const target = $((href === "#" || href === "") ? 'html' : href);
+      const position = target.offset().top;
+      $("html, body").animate({ scrollTop: position }, speed, "swing");
+      return false;
+  })
+});
+
